e4aca106f1e4d0b82a36705d7d0c3d7bcff47602
James Zhou <jam9es@gmail.com>
2025-06-24 16:56:19 -0700

    Set up project skeleton with EJS

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 // import Node dependencies
 require("dotenv").config();
-
-
+const path = require("node:path");
+const express = require("express");
 
 const app = express();
 
+// View engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 
 const PORT = process.env.PORT || 3000;
